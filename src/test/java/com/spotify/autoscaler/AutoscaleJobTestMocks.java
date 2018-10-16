f7287f0c00dc75ea8694d1e4b05d800ed3d9579d
gizem969 <gizem969@gmail.com>
2018-10-16 11:25:56 +0200

    make sure to never pass in negative duration in tests

--- src/test/java/com/spotify/autoscaler/AutoscaleJobTestMocks.java
+++ src/test/java/com/spotify/autoscaler/AutoscaleJobTestMocks.java
@@ -21,23 +21,30 @@
 package com.spotify.autoscaler;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.when;
 
 import com.google.bigtable.admin.v2.Cluster;
 import com.google.cloud.bigtable.grpc.BigtableInstanceClient;
 import com.spotify.autoscaler.client.StackdriverClient;
+import java.time.Duration;
+import org.mockito.internal.matchers.GreaterThan;
+import org.mockito.internal.matchers.LessOrEqual;
 
 public abstract class AutoscaleJobTestMocks {
+
   public static void setCurrentSize(BigtableInstanceClient client, int size) {
     Cluster cluster = Cluster.newBuilder().setServeNodes(size).build();
     when(client.getCluster(any())).thenReturn(cluster);
   }
 
   public static void setCurrentLoad(StackdriverClient client, double load) {
-    when(client.getCpuLoad(any())).thenReturn(load);
+    when(client.getCpuLoad(argThat(new GreaterThan<>(Duration.ZERO)))).thenReturn(load);
+    when(client.getCpuLoad(argThat(new LessOrEqual<>(Duration.ZERO)))).thenThrow(new RuntimeException("Negative duration!!!"));
   }
 
   public static void setCurrentDiskUtilization(StackdriverClient client, double diskUtil) {
-    when(client.getDiskUtilization(any())).thenReturn(diskUtil);
+    when(client.getDiskUtilization(argThat(new GreaterThan<>(Duration.ZERO)))).thenReturn(diskUtil);
+    when(client.getDiskUtilization(argThat(new LessOrEqual<>(Duration.ZERO)))).thenThrow(new RuntimeException("Negative duration!!!"));
   }
 }
