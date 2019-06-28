bc09ef199156f2a6b5cfd912c6a94fa67301066b
Balaji Damodaran <damodaran.balaji@gmail.com>
2019-06-28 19:55:59 +0200

    fix checkstyle error

--- src/test/java/com/spotify/autoscaler/AutoscaleJobTest.java
+++ src/test/java/com/spotify/autoscaler/AutoscaleJobTest.java
@@ -20,7 +20,10 @@
 
 package com.spotify.autoscaler;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
