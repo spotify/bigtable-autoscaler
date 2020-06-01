34387aaa5f84a54859e3ab08c45c0f7ab442f3e7
gizem969 <gizem969@gmail.com>
2020-06-01 17:56:37 +0200

    add new endpoint

--- src/main/java/com/spotify/autoscaler/di/EndpointsModule.java
+++ src/main/java/com/spotify/autoscaler/di/EndpointsModule.java
@@ -23,6 +23,7 @@ package com.spotify.autoscaler.di;
 import com.spotify.autoscaler.api.ClusterResources;
 import com.spotify.autoscaler.api.Endpoint;
 import com.spotify.autoscaler.api.HealthCheck;
+import com.spotify.autoscaler.api.KCCAutoscalerController;
 import dagger.Binds;
 import dagger.Module;
 import dagger.multibindings.IntoSet;
@@ -37,4 +38,8 @@ public abstract class EndpointsModule {
   @Binds
   @IntoSet
   public abstract Endpoint healthCheck(HealthCheck healthCheck);
+
+  @Binds
+  @IntoSet
+  public abstract Endpoint kccAutoscalerController(KCCAutoscalerController kccAutoscalerController);
 }
